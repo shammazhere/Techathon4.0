6f85fe2d8a4752f151a6013049a52ae642f8869f
haneefa96 <haneefariham39@gmail.com>
2026-05-08 06:44:30 -0700

    CROSSCHECK>>

--- backend/controllers/disaster_controller.py
+++ backend/controllers/disaster_controller.py
@@ -5,3 +5,9 @@ def get_disaster_status_data(session_id: str = "default"):
 
 def start_disaster_session_data():
     return simulation_service.start_session()
+
+def start_disaster_session_data():
+    return simulation_service.start_full_orchestration("flood")  # LangGraph!
+
+def get_disaster_status_data(session_id: str = "default"):
+    return simulation_service.get_disaster_status(session_id)
